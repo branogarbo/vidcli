1ce4d11603a8c2134ddb8c3aca6ef89bb3da0257
Brian Longmore <brianl.ext@gmail.com>
2021-04-26 20:16:09 -1000

    base project

---
+++ README.md
@@ -0,0 +1,85 @@
+<!-- # 🧬 **golcli**
+**A basic CLI implementation of Conway's Game of Life.**
+
+---
+
+## 🌱 **Setup**
+Download and compile from sources:
+```
+go get github.com/branogarbo/golcli
+```
+Install just the binary with Go:
+```
+go install github.com/branogarbo/golcli@latest
+```
+
+Or get the pre-compiled binaries for your platform on the [releases page](https://github.com/branogarbo/golcli/releases)
+
+
+## 🌳 **CLI usage**
+```
+golcli
+
+A basic CLI implementation of Conway's Game of Life.
+
+Usage:
+  golcli [flags]
+  golcli [command]
+
+Examples:
+golcli -c 100 -i 20 ./pattern.txt
+
+Available Commands:
+  build       Creates a build file from a pattern file.
+  help        Help about any command
+  run         Runs a build file.
+
+Flags:
+  -c, --count int          The number of frames displayed before exiting (-1 : infinite loop) (default -1)
+  -d, --dead-char string   The character(s) that represent a dead cell (default "██")
+  -H, --height int         The height of the frames (default 30)
+  -h, --help               help for golcli
+  -i, --interval int       The number of milliseconds between frames (default 50)
+  -l, --live-char string   The character(s) that represent a live cell (default "  ")
+  -x, --pattern-x int      The x offset of the initial pattern (default 12)
+  -y, --pattern-y int      The y offset of the initial pattern (default 8)
+  -W, --width int          The width of the frames (default 40)
+
+Use "golcli [command] --help" for more information about a command.
+```
+
+### **build:**
+```
+Creates a build file from a pattern file.
+
+Usage:
+  golcli build [flags]
+
+Examples:
+golcli build -W 70 -c 400 ./pattern.txt ./build.json
+
+Flags:
+  -c, --count int       The number of frames displayed before exiting (-1 : infinite loop) (default 1000)
+  -H, --height int      The height of the frames (default 30)
+  -h, --help            help for build
+  -x, --pattern-x int   The x offset of the initial pattern (default 12)
+  -y, --pattern-y int   The y offset of the initial pattern (default 8)
+  -W, --width int       The width of the frames (default 40)
+```
+
+### **run:**
+```
+Runs a build file.
+
+Usage:
+  golcli run [flags]
+
+Examples:
+golcli run -l "##" -i 200 ./buildFile.json
+
+Flags:
+  -d, --dead-char string   The character(s) that represent a dead cell (default "██")
+  -h, --help               help for run
+  -i, --interval int       The number of milliseconds between frames (default 50)
+  -l, --live-char string   The character(s) that represent a live cell (default "  ")
+``` -->
---
+++ cmd/root.go
@@ -0,0 +1,89 @@
+/*
+Copyright © 2021 Brian Longmore brianl.ext@gmail.com
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+)
+
+// var (
+// 	frameWidth      int
+// 	frameHeight     int
+// 	frameCount      int
+// 	patternX        int
+// 	patternY        int
+// 	patternFilePath string
+// 	buildFilePath   string
+// 	frameInterval   int
+// 	livingCellChar  string
+// 	deadCellChar    string
+// 	err             error
+// )
+
+var rootCmd = &cobra.Command{
+	Use: "golcli",
+	// Short:   "A basic CLI implementation of Conway's Game of Life.",
+	// Example: "golcli -c 100 -i 20 ./pattern.txt",
+	// Args:    cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		// patternFilePath = args[0]
+
+		// frameWidth, err = cmd.Flags().GetInt("width")
+		// frameHeight, err = cmd.Flags().GetInt("height")
+		// frameCount, err = cmd.Flags().GetInt("count")
+		// frameInterval, err = cmd.Flags().GetInt("interval")
+		// livingCellChar, err = cmd.Flags().GetString("live-char")
+		// deadCellChar, err = cmd.Flags().GetString("dead-char")
+		// patternX, err = cmd.Flags().GetInt("pattern-x")
+		// patternY, err = cmd.Flags().GetInt("pattern-y")
+
+		// if err != nil {
+		// 	fmt.Println(err)
+		// 	os.Exit(1)
+		// }
+
+		// gc := u.GameConfig{
+		// 	Width:        frameWidth,
+		// 	Height:       frameHeight,
+		// 	FrameCount:   frameCount,
+		// 	Interval:     time.Duration(frameInterval) * time.Millisecond,
+		// 	LiveCellChar: livingCellChar,
+		// 	DeadCellChar: deadCellChar,
+		// 	InitPattern: u.Pattern{
+		// 		FilePath: patternFilePath,
+		// 		X:        patternX,
+		// 		Y:        patternY,
+		// 	},
+		// }
+
+		// u.BruteRunGame(gc)
+	},
+}
+
+func Execute() {
+	cobra.CheckErr(rootCmd.Execute())
+}
+
+func init() {
+	// rootCmd.Flags().IntVarP(&frameInterval, "interval", "i", 50, "The number of milliseconds between frames")
+	// rootCmd.Flags().StringVarP(&livingCellChar, "live-char", "l", "  ", "The character(s) that represent a live cell")
+	// rootCmd.Flags().StringVarP(&deadCellChar, "dead-char", "d", "██", "The character(s) that represent a dead cell")
+	// rootCmd.Flags().IntVarP(&frameWidth, "width", "W", 40, "The width of the frames")
+	// rootCmd.Flags().IntVarP(&frameHeight, "height", "H", 30, "The height of the frames")
+	// rootCmd.Flags().IntVarP(&frameCount, "count", "c", -1, "The number of frames displayed before exiting (-1 : infinite loop)")
+	// rootCmd.Flags().IntVarP(&patternX, "pattern-x", "x", 12, "The x offset of the initial pattern")
+	// rootCmd.Flags().IntVarP(&patternY, "pattern-y", "y", 8, "The y offset of the initial pattern")
+}
